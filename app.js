3d6a21552db6c0d50d01feb06c53fcd5e34f5f5d
Viacheslav Liabin <R9514633542@gmail.com>
2024-01-17 20:49:00 +0500

    'state'

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const listElement = document.getElementById("list");
 const notes = [
   {
     title: "совершить подвиг",
-    completed: false,
+    completed: true,
   },
   {
     title: "сварить суп",
@@ -18,8 +18,9 @@ const notes = [
 ];
 
 function render() {
-  for (let note of notes) {
-    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(note));
+  listElement.innerHTML = "";
+  for (let i = 0; i < notes.length; i++) {
+    listElement.insertAdjacentHTML("beforeend", getNoteTemplate(notes[i], i));
   }
 }
 
@@ -29,25 +30,29 @@ createBtn.onclick = function () {
   if (inputElement.value.length === 0) {
     return;
   }
-  
+
   const newNote = {
     title: inputElement.value,
     completed: false,
   };
-
-  listElement.insertAdjacentHTML("beforeend", getNoteTemplate(newNote));
+  notes.push(newNote);
+  render();
   inputElement.value = "";
 };
 
-function getNoteTemplate(note) {
+function getNoteTemplate(note, index) {
   return `
   <li 
     class="list-group-item d-flex justify-content-between 
     align-items-center"
   >
-  <span>${note.title}</span>
+  <span class="${note.completed ? "text-decoration-line-through" : ""}">${
+    note.title
+  }</span>
   <span>
-      <span class="btn btn-small btn-success">&check;</span>
+      <span class="btn btn-small btn-${
+        note.completed ? "warning" : "success"
+      }" data-index="${index}">&check;</span>
       <span class="btn btn-small btn-danger">&times;</span>
   </span>
 </li>`;
